b8c9f4c2d6234efad96612f63f338a8fa064150a
Ankit <ankitchohan61@gmail.com>
2023-10-03 22:17:04 +0530

    questions added

--- Client/src/Component/Home.js
+++ Client/src/Component/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { fechtTechnologies } from '../hooks/technologyHook';
-
+import axios from 'axios';
 function Home() {
     const [technologies, setTechnologies] = useState([]);
     const [selectedTechnology, setSelectedTechnology] = useState('');
+    const [ questionSelected , setQuestionSelected] = useState([])
+    const [answer , setAnswer] = useState("")
+    const [showAnswer , setShowAnswer] = useState("")
 
     useEffect(() => {
         fechtTechnologies().then(resp => setTechnologies(resp.data)).catch(err => console.log("Error fetching error", err));
@@ -12,17 +15,25 @@ function Home() {
 
     const handleTechnologyChange = (event) => {
       setSelectedTechnology(event.target.value);
+      debugger;
+      axios.post("http://localhost:8080/question",{technology : event.target.value}).then(resp=>setQuestionSelected(resp.data)).catch(err=>console.log("Error ",err))
     };
+    const handleAnswer = (event)=>{
+        debugger;
+        setAnswer(event.target.value);
+        const filteredData  =  questionSelected.filter((item,index)=>item._id===event.target.value);
+        setShowAnswer(filteredData[0].answer);
+    }
   
     return (
         <>
             <div style={{ display: 'flex', marginTop: '3%' }}>
                 <div className="form-container" style={{ marginLeft: '5%', marginRight: '3%' }}>
                     <div className="form-group" style={{ marginTop: '12%', width: 400 }}>
-                        <label htmlFor="technology"><b>Technologies</b></label>
+                        <label htmlFor="technology" className='mb-2'><b>Technologies</b></label>
                         <select className="form-select" id="technology" 
                                 value={selectedTechnology}
-                                onChange={handleTechnologyChange}                        
+                                onChange={handleTechnologyChange}
                         >
                             <option>Select Technology</option>
                             {technologies.length > 0
@@ -36,18 +47,19 @@ function Home() {
                     </div>
 
                     <div className="form-group" style={{ marginTop: '18%', width: 400 }}>
-                        <label htmlFor="question"><b>Questions</b></label>
-                        <select className="form-select" id="question">
-                            <option value="reacttutorial">ReactTutorial</option>
-                            <option>Select Question</option>
-                            <option value="SQL">SQL</option>
+                        <label htmlFor="question" className='mb-2'><b>Questions</b></label>
+                        <select className="form-select" id="question"  value={answer} onChange={handleAnswer}>
+                            <option value="reacttutorial">Select Question</option>
+                         {questionSelected.length>0 ? 
+                          questionSelected.map(item=>
+                            <option key={item._id} value={item._id}>{item.question}</option>) : "Loading..."}
                         </select>
                     </div>
                 </div>
 
                 <div className="form-group" style={{ marginLeft: '4%', width: 600, marginTop: '3%' }}>
-                    <label htmlFor="answer"><b>Answers</b></label>
-                    <textarea className="form-control" id="answer" rows="12"></textarea>
+                    <label htmlFor="answer" className='mb-2'><b>Answers</b></label>
+                    <textarea className="form-control" id="answer" rows="12" value={showAnswer} ></textarea>
                 </div>
             </div>
         </>
--- Client/src/Component/QA.js
+++ Client/src/Component/QA.js
@@ -1,30 +1,83 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { fechtTechnologies } from '../hooks/technologyHook';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
+
 
 function QA() {
+    const [technologies, setTechnologies] = useState([]);
+    const [selectedTechnology, setSelectedTechnology] = useState('');
+    const [question, setQuestion] = useState("")
+    const [answer, setAnswer] = useState("")
+
+    useEffect(() => {
+        fechtTechnologies().then(resp => setTechnologies(resp.data)).catch(err => console.log("Error fetching error", err));
+    }, [])
+
+
+    const handleTechnologyChange = (event) => {
+        setSelectedTechnology(event.target.value);
+    };
+
+    const handleSubmit = async () => {
+        const data = {
+            question,
+            answer,
+            technology: selectedTechnology
+        }
+        const result = await axios.post("http://localhost:8080/question/new", data).then(response => {
+            if (response.data == "Question added successfully") {
+                toast.success("Question added Successfully")
+            }
+        }).catch(err => console.log("Error", err))
+        console.log("Result Post", result);
+        setQuestion("");
+        setAnswer("");
+        setSelectedTechnology("");
+    }
+
     return (
         <div>
             <br />
+            <Toaster />
+            <Row style={{ marginTop: '5px', marginBottom: '2%' }}>
+                <Col xs={6} className="mx-auto">
+                    <label htmlFor="technology" className='mb-2'><b>Technologies</b></label>
+                    <select className="form-select" id="technology" value={selectedTechnology}
+                        onChange={handleTechnologyChange}>
+                        <option>Select Technology</option>
+                        {technologies.length > 0
+                            ? technologies.map(item => (
+                                <option key={item._id} value={item.name}>
+                                    {item.name}
+                                </option>
+                            ))
+                            : <option>Loading...</option>}
+                    </select>
+                </Col>
+            </Row>
             <div>
-                <Row style={{ marginTop: '5px' }}>
+                <Row style={{ marginTop: '5px', marginBottom: '2%' }}>
                     <Col xs={6} className="mx-auto">
-                        <label htmlFor="">Question</label>
-                        <input type="text" placeholder="Enter Question" className="form-control" />
+                        <label htmlFor=""><b>Question</b></label>
+                        <input type="text" placeholder="Enter Question" className="form-control" value={question} onChange={(event) => setQuestion(event.target.value)} />
                     </Col>
                 </Row>
             </div>
             <div>
-                <Row style={{ marginTop: '5px' }}>
+                <Row style={{ marginBottom: '2%' }}>
                     <Col xs={6} className="mx-auto">
-                        <label className="form-label">Answer</label>
-                        <textarea style={{ width: 800 }} placeholder="Enter Answer" className="form-control" rows="8"></textarea>
+                        <label className="form-label"><b>Answer</b></label>
+                        <textarea style={{ width: 620 }} placeholder="Enter Answer" className="form-control" rows="8" onChange={(event) => setAnswer(event.target.value)} value={answer} ></textarea>
                     </Col>
                 </Row>
             </div>
             <br />
             <div className="text-center">
-                <button className="btn btn-success">Submit</button>
+                <button className="btn btn-success" onClick={() => handleSubmit()}>Submit</button>
             </div>
         </div>
     )
